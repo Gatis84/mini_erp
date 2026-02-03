2b180ba364dc3f4b9ae125f0914afdcfa9b09351
i_code <fullstack_61@proton.me>
2026-02-03 17:21:05 +0200

    Implemented My Task Assignment Status change option

--- src/frontend/views/Task/view.php
+++ src/frontend/views/Task/view.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\helpers\Url;
 
 /** @var yii\web\View $this */
 /** @var common\models\Task $model */
@@ -10,7 +11,33 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Tasks', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+
+$updateUrl = Url::to(['task-assignment/update-status-ajax']);
+
+$js = <<<JS
+$('.assignment-status').on('change', function () {
+    const el = $(this);
+
+    $.post('$updateUrl', {
+        id: el.data('id'),
+        status: el.val(),
+        _csrf: yii.getCsrfToken()
+    }).done(function (res) {
+        if (res && res.success) {
+            location.reload();
+        } else {
+            alert('Status update failed');
+        }
+    }).fail(function () {
+        alert('Status update failed');
+    });
+});
+JS;
+
+$this->registerJs($js);
+
 ?>
+
 <div class="task-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -37,18 +64,84 @@ $this->params['breadcrumbs'][] = $this->title;
                 'label' => 'Assigned Employees',
                 'value' => function ($model) {
                     return implode(', ', array_map(
-                        fn($a) => $a->employee->first_name . ' ' . $a->employee->last_name,
+                        fn($a) => $a->employee->first_name . ' ' . $a->employee->last_name . '(Task Assignment status: ' . $a->getStatusLabel() . ')',
                         $model->assignments
                     ));
                 },
             ],
             [
                 'attribute' => 'status',
+                'label' => 'Task status',
                 'value' => fn($m) => $m->getStatusLabel(),
             ],
-            'created_by',
-            'created_at',
-            'updated_at',
+            [
+                'label' => 'My Task Assignment Status',
+                'format' => 'raw',
+                'value' => function ($model) {
+
+                    $employee = \common\models\Employee::findOne([
+                        'user_id' => Yii::$app->user->id
+                    ]);
+
+                    if (!$employee) {
+                        return 'N/A';
+                    }
+
+                    $assignment = \common\models\TaskAssignment::findOne([
+                        'task_id' => $model->id,
+                        'employee_id' => $employee->id,
+                    ]);
+
+                    if (!$assignment) {
+                        return 'Not assigned';
+                    }
+
+                    if (!Yii::$app->user->can('taskAssignment.updateOwnStatus', [
+                        'model' => $assignment,
+                    ])) {
+                        return $assignment->getStatusLabel();
+                    }
+
+                    return Html::dropDownList(
+                        'assignment_status',
+                        $assignment->status,
+                        $assignment->statusList(),
+                        [
+                            'class' => 'form-control assignment-status',
+                            'data-id' => $assignment->id,
+                            'style' => 'width:180px',
+                        ]
+                    );
+                },
+            ],
+            [
+                'label' => 'Created by',
+                'value' => function ($model) {
+                    $creator = $model->getCreatorData();
+                    return $creator ? 'ID:' . $creator['user_id'] . ' ' . $creator['first_name'] . ' ' . $creator['last_name'] : 'sysAdmin';
+                },
+            ],
+            'created_at:datetime',
+            'updated_at:datetime',
+            [
+                'label' => 'Planned Start At',
+                'value' => function ($model) {
+                    return $model->planned_start_at ? Yii::$app->formatter->asDatetime($model->planned_start_at) : 'Not set';
+                },
+            ],
+            [
+                'label' => 'Planned End At',
+                'value' => function ($model) {
+                    return $model->planned_end_at ? Yii::$app->formatter->asDatetime($model->planned_end_at) : 'Not set';
+                },
+
+            ],
+            [
+                'label' => 'Completed At',
+                'value' => function ($model) {
+                    return $model->completed_at ? Yii::$app->formatter->asDatetime($model->completed_at) : 'Not completed';
+                },
+            ]
         ],
     ]) ?>
 
